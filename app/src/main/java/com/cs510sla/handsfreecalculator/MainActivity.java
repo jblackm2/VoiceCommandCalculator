7db02bc21623fa2a7e038bd4d54b90289021e986
Justin Blackmon <jblackm2@pdx.edu>
2016-11-07 10:22:19 -0800

    Initial commit

---
+++ app/src/main/java/com/cs510sla/handsfreecalculator/MainActivity.java
@@ -0,0 +1,25 @@
+package com.cs510sla.handsfreecalculator;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button startButton = (Button) findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,CalcActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
