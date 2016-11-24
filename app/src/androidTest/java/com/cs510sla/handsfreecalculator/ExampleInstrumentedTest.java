e8dfe1531b97c159e32a2844598727f1c2fd799a
Justin Blackmon <jblackm2@pdx.edu>
2016-11-23 19:14:22 -0800

    Comment out, will fix failing parts later.

--- app/src/androidTest/java/com/cs510sla/handsfreecalculator/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/cs510sla/handsfreecalculator/ExampleInstrumentedTest.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,11 +18,34 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
+    Context appContext;
+    @Before
+    public void init(){
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
+    }
+    @Test
+    public void useAppContext() throws Exception {
+
 
         assertEquals("com.cs510sla.handsfreecalculator", appContext.getPackageName());
     }
+    @Test
+    public void testSetAndGet(){
+        /*
+        appContext.
+        appContext.setAnswerText("1+2*3/4.5%6+-7");
+        String res = "";
+        res = calc.getAnswerText();
+        Assert.assertEquals("1+2*3/4.5%6+-7", res);
+    }
+    @Test
+    public void testSetAndClear(){
+        calc.setAnswerText("1+2*3/4.5%6+-7");
+        String res = "";
+        calc.clearText();
+        res = calc.getAnswerText();
+        Assert.assertEquals("", res);
+    */}
+
 }
